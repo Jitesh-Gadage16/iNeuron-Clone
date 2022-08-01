a5e3f1a01e7a5a9f72e181b988848f98fb3c8c4c
Jitesh Gadage <gadagejitesh16@gmail.com>
2022-08-01 10:00:25 +0530

    courses section added

---
+++ script.js
@@ -0,0 +1,9 @@
+// $('.single-item').slick();
+const el = document.getElementById('hoverdiv');
+
+const hiddenDiv = document.getElementById('courde-disc');
+
+// ✅ Show hidden DIV on hover
+el.addEventListener('mouseover', function handleMouseOver() {
+//   hiddenDiv.style.display = 'block';
+console("bhai chal rha")
