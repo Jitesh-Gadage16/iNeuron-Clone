e4885affc90715703996fafa49cd0073b72a11aa
Jitesh Gadage <gadagejitesh16@gmail.com>
2022-08-03 10:24:17 +0530

    chnages

--- script.js
+++ script.js
@@ -15,14 +15,14 @@
 
 
 
-$('.count').each(function () {
-    $(this).prop('Counter',0).animate({
-        Counter: $(this).text()
-    }, {
-        duration: 3000,
-        easing: 'swing',
-        step: function (now) {
-            $(this).text(Math.ceil(now));
-        }
-    });
-});
+// $('.count').each(function () {
+//     $(this).prop('Counter',0).animate({
+//         Counter: $(this).text()
+//     }, {
+//         duration: 3000,
+//         easing: 'swing',
+//         step: function (now) {
+//             $(this).text(Math.ceil(now));
+//         }
+//     });
+// });
