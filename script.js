837be6ba3c35c019f42111b8e1293f7df0a42621
Jitesh Gadage <gadagejitesh16@gmail.com>
2022-08-04 01:31:57 +0530

    done with the slider bug fix

--- script.js
+++ script.js
@@ -26,3 +26,31 @@
 //         }
 //     });
 // });
+
+
+// $(document).ready(function(){
+//     $('.tech-avengers-div').slick({
+//       slidesToShow: 3,
+//       slidesToScroll: 1,
+//       arrows: true,
+//       dots: true,
+//       speed: 300,
+//       infinite: true,
+//       autoplaySpeed: 5000,
+//       autoplay: true,
+//       responsive: [
+//     {
+//       breakpoint: 991,
+//       settings: {
+//         slidesToShow: 3,
+//       }
+//     },
+//     {
+//       breakpoint: 767,
+//       settings: {
+//         slidesToShow: 1,
+//       }
+//     }
+//   ]
+//     });
+//   });
